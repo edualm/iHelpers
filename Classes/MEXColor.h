548dc7e37dc7ea5396e63bfc58997173949ea5db
Eduardo Almeida <megaedux@me.com>
2010-09-05 22:28:54 +0100

    Updated MEXCracking

---
+++ Classes/MEXColor.h
@@ -0,0 +1,16 @@
+//
+//  MEXColor.h
+//  iHelpers
+//
+//  Created by Eduardo Almeida on 10/09/05.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MEXColor : NSObject {
+
+}
+
+@end
