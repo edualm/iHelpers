11b97f13110c6e15cac3e49b2acbbe2d931e75cd
Eduardo Almeida <megaedux@me.com>
2010-11-14 19:32:25 +0000

    Added MEXGameCenter class/import on iHelpers.h

--- Classes/iHelpers.h
+++ Classes/iHelpers.h
@@ -11,10 +11,12 @@
 #import "MPMoviePlayerController+iHelpers.h"
 #import "AVAudioPlayer+iHelpers.h"
 #import "MEXRandom.h"
+#import "MEXGameCenter.h"
 
 @class MEXCracking;
 @class MEXMultitasking;
 @class MEXLog;
 @class MEXRandom;
+@class MEXGameCenter;
 
 // MEXLog and the AVAudioPlayer forward class are untested code. Use at your own risk.
